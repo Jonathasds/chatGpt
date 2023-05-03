c6f30be4b3d7f44d37558aa755da6d51882f0727
Jônathas Delgado da silva <83735539+Jonathasds@users.noreply.github.com>
2023-05-03 20:28:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ inputQuestion.addEventListener("keypress", (e) => {
   if (inputQuestion.value && e.key === "Enter") SendQuestion();
 });
 
-const OPENAI_API_KEY = "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";
+const OPENAI_API_KEY = "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";
 
 function SendQuestion() {
   var sQuestion = inputQuestion.value;
